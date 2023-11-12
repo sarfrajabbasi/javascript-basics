0384884b0cef2dd25950d372066b05cb06aa1c60
sarfrajabbasi <sarfrajk96811@gmail.com>
2023-11-12 08:19:36 +0530

    javascript

--- JS/part-2.js
+++ JS/part-2.js
@@ -17,3 +17,27 @@ console.log(apples);
 
 var apples = 56 - 3;
 console.log(apples);
+
+var chops =7;
+chops++;
+chops--;
+console.log(chops);
+
+// assigment operator
+var bear = 23;
+bear +=1;
+bear -=1;
+bear %=1;
+bear *=1;
+bear /=1;
+
+// if statement
+
+var apples =53;
+var hotdogs = 53;
+
+if(apples === hotdogs){
+console.log("Yayyy it's worked!");
+}
+
+// if/else statement
