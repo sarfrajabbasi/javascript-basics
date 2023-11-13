1c6ee2e71ebaf6c9d19d299d4d8f0f10d9970a78
sarfrajabbasi <sarfrajk96811@gmail.com>
2023-11-13 11:32:58 +0530

    javascript

--- JS/part-3.js
+++ JS/part-3.js
@@ -9,11 +9,7 @@ const val = document.getElementById('value');
 const submit = document.getElementById('submit');
 const img = document.getElementById('img');
 
-submit.addEventListener('click',function(){
+function changes(){
     let currVal = val.value;
     img.src= `https://source.unsplash.com/random/300x300/?${currVal}`
-})
-
-function changes(){
-
 }
