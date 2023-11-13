a3670c78d6162a60a03c6245b4d4cfd50d876e09
sarfrajabbasi <sarfrajk96811@gmail.com>
2023-11-13 11:26:44 +0530

    javascript

--- JS/part-3.js
+++ JS/part-3.js
@@ -3,3 +3,17 @@
 function touchMe(){
     console.log("hello");
 }
+
+// onMouseOver & onLoad
+const val = document.getElementById('value');
+const submit = document.getElementById('submit');
+const img = document.getElementById('img');
+
+submit.addEventListener('click',function(){
+    let currVal = val.value;
+    img.src= `https://source.unsplash.com/random/300x300/?${currVal}`
+})
+
+function changes(){
+
+}
